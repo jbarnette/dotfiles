361ce9c17498d325998a9e049321ca1cba1517d4
John Barnette <jbarnette@gmail.com>
2009-02-02 22:48:05 -0800

    updates for bash and autotest

---
+++ bash/aliases.sh
@@ -0,0 +1,2 @@
+alias jek='jekyll --pygments --auto --server'
+alias e="$EDITOR -n"
--- bash/main.sh
+++ bash/main.sh
@@ -1,4 +1,8 @@
 export PATH=`tr '\n' : < $DOTFILES/bash/paths.txt`
 
 . "$DOTFILES/bash/git.sh"
+. "$DOTFILES/bash/aliases.sh"
+
 export PS1='$(__git_ps1 "[%s] ")\w\\$ '
+export LD_LIBRARY_PATH=/opt/local/lib
+export EDITOR=/usr/local/bin/emacsclient
