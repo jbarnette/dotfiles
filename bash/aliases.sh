418d8f1bc8d426e5e0a439ca428a36c46c8d425c
John Barnette <jbarnette@gmail.com>
2009-02-13 15:02:28 -0800

    Pull in a few of the old aliases.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,3 +1,7 @@
 alias e="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
 alias jek="jekyll --pygments --auto --server"
+
+# ruby and rails bits
 alias rake="rake -qs"
+alias rake1.9="rake1.9 -qs"
+alias ss="script/server"
