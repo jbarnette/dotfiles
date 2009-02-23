6cf2d43c9924af4b63c7eeec61ffeb7afabfc19a
John Barnette <jbarnette@gmail.com>
2009-02-23 14:03:40 -0800

    Hardcode DOTFILES.

--- bash/main.sh
+++ bash/main.sh
@@ -1,8 +1,10 @@
+export DOTFILES=~/work/dotfiles
 export PATH=`tr '\n' : < $DOTFILES/bash/paths.txt`
 
 . "$DOTFILES/bash/git.sh"
 . "$DOTFILES/bash/aliases.sh"
 
+export MANPATH=/opt/local/share/man:$MANPATH
 export PS1='$(__git_ps1 "[%s] ")\w\\$ '
 export LD_LIBRARY_PATH=/opt/local/lib
 export EDITOR=/Applications/Emacs.app/Contents/MacOS/bin/emacsclient
