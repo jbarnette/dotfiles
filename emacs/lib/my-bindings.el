e24addbeeff54e43917889ec7c40a627444c5ec1
John Barnette <jbarnette@gmail.com>
2009-06-07 03:11:10 -0700

    Go to file at point.

--- emacs/lib/my-bindings.el
+++ emacs/lib/my-bindings.el
@@ -2,5 +2,6 @@
 (global-set-key (kbd "C-.")          'jbarnette-ido-goto-symbol)
 (global-set-key (kbd "C-<return>")   'jbarnette-next-line)
 (global-set-key (kbd "C-M-<return>") 'jbarnette-prev-line)
+(global-set-key (kbd "C-X f")        'find-file-at-point)
 
 (provide 'my-bindings)
