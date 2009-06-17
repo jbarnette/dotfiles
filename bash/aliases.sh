da1c99cd31235fe4bb321e397dccd75c56b2c459
John Barnette <jbarnette@gmail.com>
2009-06-17 14:05:12 -0700

    Remove some obsolete aliases.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -5,13 +5,3 @@ alias pj="pastejour"
 # ruby and rails bits
 alias rake="rake -qs"
 alias rake1.9="rake1.9 -qs"
-alias ss="script/server"
-
-# latest versions of unreleased stuff
-for f in `find ~/work/p4/src -type f | grep /dev/bin/`
-do
-    d=`dirname $f`
-    alias dev-`basename $f`="ruby -I$d/../lib:$d/../ext -S $f"
-done
-
-alias my-sow="ruby -I~/work/hoe/lib -S ~/work/hoe/bin/sow"
