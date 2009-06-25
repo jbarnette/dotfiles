d3ad2d1977fca6188c7089061c2b9ba26d4934f6
John Barnette <jbarnette@gmail.com>
2009-06-25 10:31:18 -0700

    Add an alias for MacRake.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,5 +1,6 @@
 alias e="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
 alias gitify="git svn clone -s --prefix=svn/"
+alias mr="macrake -qs"
 alias pj="pastejour"
 
 # ruby and rails bits
