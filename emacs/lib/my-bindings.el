823a15a3e7a2ca3b0744697169370310f2e8c9d6
John Barnette <jbarnette@gmail.com>
2009-10-07 18:14:46 -0700

    Bind C-x m to magit-status. I don't use Emacs for mail.

--- emacs/lib/my-bindings.el
+++ emacs/lib/my-bindings.el
@@ -1,8 +1,9 @@
-(global-set-key (kbd "C-M-h")        'backward-kill-word)
 (global-set-key (kbd "C-.")          'jbarnette-ido-goto-symbol)
 (global-set-key (kbd "C-<return>")   'jbarnette-next-line)
 (global-set-key (kbd "C-M-<return>") 'jbarnette-prev-line)
-(global-set-key (kbd "C-X f")        'find-file-at-point)
+(global-set-key (kbd "C-M-h")        'backward-kill-word)
+(global-set-key (kbd "C-x f")        'find-file-at-point)
+(global-set-key (kbd "C-x m")        'magit-status)
 (global-set-key (kbd "M-s")          'fixup-whitespace)
 
 (require 'toggle)
