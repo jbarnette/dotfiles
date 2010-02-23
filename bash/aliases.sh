7ff9cc704a8091f203484b6739ccdbe1542607de
John Barnette <jbarnette@gmail.com>
2010-02-23 15:44:29 -0800

    Use hub.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,4 +1,5 @@
 alias e="/Applications/MacPorts/Emacs.app/Contents/MacOS/bin/emacsclient -n"
+alias git=hub
 alias gitify="git svn clone -s --prefix=svn/"
 alias mr="macrake -qs"
 alias pj="pastejour"
