76af7b55a9d36f99b51660e2207fd34fd12a6c30
John Barnette <jbarnette@gmail.com>
2010-06-27 20:09:23 -0700

    Kill js2-mode insanity.

--- emacs/lib/my-modes.el
+++ emacs/lib/my-modes.el
@@ -3,9 +3,6 @@
 (setq auto-mode-alist
    (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
 
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
 (autoload 'yaml-mode "yaml-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
