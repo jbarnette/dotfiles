1581eeeb233c21496ce9c9ba5889164b3bb56f90
John Barnette <jbarnette@gmail.com>
2011-02-12 10:58:02 -0600

    Kill old aliases.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,9 +1,3 @@
 alias e="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -n"
 alias git=hub
-alias gitify="git svn clone -s --prefix=svn/"
-alias mr="macrake -qs"
-alias pj="pastejour"
-
-alias drake="ruby -d -S rake"
 alias rake="rake -qs"
-alias rake1.9="rake1.9 -qs"
