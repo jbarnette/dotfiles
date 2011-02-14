bea017d7b912b9b5d8d6d795828665ed410abb28
John Barnette <jbarnette@gmail.com>
2011-02-13 19:13:53 -0600

    Don't use nonstandard ln options.

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 git submodule update --init
 
-ln -nfs ~/my/dotfiles/bash/main.sh ~/.profile
-ln -nfs ~/.profile ~/.bashrc
-ln -nfs ~/.profile ~/.bash_profile
-ln -nfs ~/my/dotfiles/emacs ~/.emacs.d
-ln -nfs ~/my/dotfiles/hoe/hoerc ~/.hoerc
-ln -nfs ~/my/dotfiles/hoe/template ~/.hoe_template
+ln -s ~/my/dotfiles/bash/main.sh ~/.profile
+ln -s ~/.profile ~/.bashrc
+ln -s ~/.profile ~/.bash_profile
+ln -s ~/my/dotfiles/emacs ~/.emacs.d
+ln -s ~/my/dotfiles/hoe/hoerc ~/.hoerc
+ln -s ~/my/dotfiles/hoe/template ~/.hoe_template
 
 . ~/.profile
+
