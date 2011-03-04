9156c271df0363ebf6c9122d541718b2f15c0892
John Barnette <jbarnette@gmail.com>
2011-03-04 11:11:31 -0600

    Add some common bindings.

--- emacs/jbarnette.el
+++ emacs/jbarnette.el
@@ -1,1 +1,5 @@
 (put 'erase-buffer 'disabled nil)
+
+(global-set-key (kbd "C-x f") 'find-file-at-point)
+(global-set-key (kbd "C-x m") 'magit-status)
+(global-set-key (kbd "M-s")   'fixup-whitespace)
