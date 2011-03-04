e828948ea3d7b9e6328ccc4581b354db956fd511
John Barnette <jbarnette@gmail.com>
2011-03-04 11:17:31 -0600

    Ruby electric mode and the emacs server.

--- emacs/jbarnette.el
+++ emacs/jbarnette.el
@@ -3,3 +3,8 @@
 (global-set-key (kbd "C-x f") 'find-file-at-point)
 (global-set-key (kbd "C-x m") 'magit-status)
 (global-set-key (kbd "M-s")   'fixup-whitespace)
+
+(add-hook 'ruby-mode-hook 'ruby-electric-mode)
+
+(setenv "PAGER" "/bin/cat")
+(server-start)
