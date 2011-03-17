a769d3f7095e8e5a359d39850ac08e1a8a4a6bab
John Barnette <jbarnette@gmail.com>
2011-03-17 11:52:49 -0500

    Update main BASH file.

--- bash/main.sh
+++ bash/main.sh
@@ -1,23 +1,26 @@
+source /etc/profile
+
 export DOTFILES=~/my/dotfiles
 
-PATH=~/bin:$PATH:`tr '\n' : < $DOTFILES/bash/paths.txt`
+PATH=`tr '\n' : < $DOTFILES/bash/paths.txt`
 
 export PATH=`ruby -e "print ENV['PATH'].split(':'). \
     reject { |p| p.nil? || p.empty? }.uniq. \
     select { |p| File.directory? File.expand_path(p) }.join ':'"`
 
-. "$DOTFILES/bash/aliases.sh"
-. "$DOTFILES/bash/git.sh"
+source "$DOTFILES/bash/aliases.sh"
+source "$DOTFILES/bash/git.sh"
 
 if [ -s "$DOTFILES/bash/private.sh" ]
   then source "$DOTFILES/bash/private.sh"
 fi
 
-export EDITOR=/Applications/Emacs.app/Contents/MacOS/bin/emacsclient
+export EDITOR=/usr/local/bin/emacs
 export VISUAL=$EDITOR
 
 export ARCHFLAGS="-arch x86_64"
 export JAVA_HOME=`/usr/libexec/java_home`
-export P4CONFIG=.p4config
-export PS1='$(__git_ps1 "[%s] ")\w\\$ '
+export  P4CONFIG=.p4config
+export       PS1='$(__git_ps1 "[%s] ")\w\\$ '
 export NODE_PATH="/usr/local/lib/node"
+
