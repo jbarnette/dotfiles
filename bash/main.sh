ccaff920fbfa4103b555c65c9c2a86e453b0a7ff
John Barnette <jbarnette@gmail.com>
2011-03-19 13:25:08 -0500

    Simplify paths.

--- bash/main.sh
+++ bash/main.sh
@@ -2,11 +2,7 @@ source /etc/profile
 
 export DOTFILES=~/my/dotfiles
 
-PATH=`tr '\n' : < $DOTFILES/bash/paths.txt`
-
-export PATH=`ruby -e "print ENV['PATH'].split(':'). \
-    reject { |p| p.nil? || p.empty? }.uniq. \
-    select { |p| File.directory? File.expand_path(p) }.join ':'"`
+export PATH= ~/bin:$PATH
 
 source "$DOTFILES/bash/aliases.sh"
 source "$DOTFILES/bash/git.sh"
@@ -23,4 +19,3 @@ export JAVA_HOME=`/usr/libexec/java_home`
 export  P4CONFIG=.p4config
 export       PS1='$(__git_ps1 "[%s] ")\w\\$ '
 export NODE_PATH="/usr/local/lib/node"
-
