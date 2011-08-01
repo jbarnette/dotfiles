cbb16501fd336dd587a6459896f4a47630575a0a
John Barnette <john@jbarnette.com>
2011-08-01 12:42:27 -0700

    Get some religion.

---
+++ emacs/jbarnette/religion.el
@@ -0,0 +1,8 @@
+(setq-default indent-tabs-mode nil)
+
+(setq whitespace-style '(tabs trailing lines)
+      whitespace-line-column 80
+      line-number-mode t
+      column-number-mode t)
+
+(global-whitespace-mode t)
