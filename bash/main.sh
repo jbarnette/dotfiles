9005c3837b8a7a92552d45b9190edaf04eb40588
John Barnette <jbarnette@github.com>
2013-06-25 20:52:22 -0700

    Load app-env if it exists

--- bash/main.sh
+++ bash/main.sh
@@ -7,12 +7,17 @@ if [ -s "$DOTFILES/bash/private.sh" ]
   then source "$DOTFILES/bash/private.sh"
 fi
 
-export EDITOR="emacsclient"
-export NODE_NO_READLINE=1
-export PATH=$HOME/bin:$PATH
-export PS1='$(__git_ps1 "[%s] ")\w\\$ '
-export VISUAL=$EDITOR
-
 if [ -f "/opt/github/env.sh" ]
   then source "/opt/github/env.sh"
 fi
+
+if [ -f $HOME/src/app-env ]; then
+  export PATH="$HOME/src/app-env/bin:$PATH"
+  source "$HOME/src/app-env/share/app-ps1.sh"
+fi
+
+export EDITOR="atom --wait"
+export NODE_NO_READLINE=1
+export PATH=$HOME/bin:$PATH
+export PS1='$(app-ps1 "%s ")$(__git_ps1 "[%s] ")\w\\$ '
+export VISUAL=$EDITOR
