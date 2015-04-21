983c99fdf053ff8a4a93ff2f957003aebd5f82ba
John Barnette <jbarnette@github.com>
2015-04-20 19:40:30 -0500

    Spring cleaning

--- bash/main.sh
+++ bash/main.sh
@@ -2,19 +2,25 @@ if [ -f "/opt/boxen/env.sh" ]
   then source "/opt/boxen/env.sh"
 fi
 
-export DOTFILES=~/my/dotfiles
-
-source "$DOTFILES/bash/functions.sh"
+export DOTFILES=~/src/jbarnette/dotfiles
 
 if [ -s "$DOTFILES/bash/private.sh" ]; then
   source "$DOTFILES/bash/private.sh"
 fi
+
+for file in $DOTFILES/bash/initializers/*.sh; do
+  source "$file"
+done
 
 export EDITOR="atom --wait"
 export GOPATH=$HOME/go
 export NODE_NO_READLINE=1
 export PATH=$HOME/bin:$GOPATH/bin:$PATH
 export PROMPT_COMMAND="set-terminal-title"
-export PS1="❤ "
+export PS1="\$ "
 export SRCPATH=$HOME/src
 export VISUAL=$EDITOR
+
+if which hub > /dev/null; then
+  alias git=hub
+fi
