2a443564283882828eb18ce3df4d3c6ba060fe55
John Barnette <jbarnette@users.noreply.github.com>
2015-07-08 17:13:34 -0500

    Source initializers after basics

--- bash/main.sh
+++ bash/main.sh
@@ -8,10 +8,6 @@ if [ -s "$DOTFILES/bash/private.sh" ]; then
   source "$DOTFILES/bash/private.sh"
 fi
 
-for file in $DOTFILES/bash/initializers/*.sh; do
-  source "$file"
-done
-
 export EDITOR="atom --wait"
 export GOPATH=$HOME/go
 export NODE_NO_READLINE=1
@@ -20,6 +16,10 @@ export PROMPT_COMMAND="set-terminal-title"
 export PS1="\$ "
 export SRCPATH=$HOME/src
 export VISUAL=$EDITOR
+
+for file in $DOTFILES/bash/initializers/*.sh; do
+  source "$file"
+done
 
 if which hub > /dev/null; then
   alias git=hub
