b6d5de03412844c26303d89a3d290451306177a1
John Barnette <jbarnette@github.com>
2015-09-01 09:58:38 -0500

    :bowtie:

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#/ Link dotfiles and install apps.
-#/ Usage: ./install.sh
-
-set -e
-
-ln -nfs ~/src/jbarnette/dotfiles/bash/main.sh ~/.profile
-ln -nfs ~/src/jbarnette/dotfiles/gitconfig ~/.gitconfig
